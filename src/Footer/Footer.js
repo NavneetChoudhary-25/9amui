d25bd3c1e92085b91868b4b14459e8365083b4de
navneet choudhary <navneetchoudhary90980@gmail.com>
2024-01-10 01:07:10 +0530

    header footer changes

---
+++ src/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='position-fixed bottom-0 bg-primary text-center text-white w-100'>
+        &copy; right belongs to me
+      
+    </div>
+  )
+}
+
+
---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export const Header = () => {
+  return (
+    <div className={`bg-primary text-white text-center ${styles.header}`}>
+        My First App
+      
+    </div>
+  )
+}
