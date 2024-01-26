a971d4432e9b143b4643a188617a89123feecfd2
navneet choudhary <navneetchoudhary90980@gmail.com>
2024-01-26 19:14:15 +0530

    build fix

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -9,62 +9,70 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Cookies } from '@/common/cookies';
+import { useRouter } from 'next/navigation';
 
 const Users = () => {
-    const [users, setUsers] = useState([])
-    const headers = ["Name", "User ID", "Address"]
-    const columns = ["name", "uid", "address"]
-    const dispatch = useDispatch();
-    useEffect(() => {
-        fnGetUsers();
-    }, [])
-    const fnGetUsers = async () => {
+  const [users, setUsers] = useState([])
+  const router = useRouter();
+  const headers = ["Name", "User ID", "Address"]
+  const columns = ["name", "uid", "address"]
+  const dispatch = useDispatch();
 
-        try {
-            dispatch({ type: "LOADER", payload: true })
-            const res = await Api.fnSendGetReq('std/get-std')
-            setUsers(res?.data)
-        } catch (ex) {
-            console.error("users", ex);
-            setUsers([])
-        } finally {
-            dispatch({ type: "LOADER", payload: false })
-        }
+  if (!Cookies.hasActiveSession()) {
+    router.push('/')
+  }
 
+  useEffect(() => {
+    fnGetUsers();
+  }, [])
+
+  const fnGetUsers = async () => {
+    try {
+      dispatch({ type: "LOADER", payload: true })
+      const res = await Api.fnSendGetReq('std/get-std')
+      setUsers(res?.data || [])
+    } catch (ex) {
+      console.error("users", ex);
+      setUsers([])
+    } finally {
+      dispatch({ type: "LOADER", payload: false })
     }
-    return (
-        <div>
-             <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableRow>
-            {
-                headers.map((val, ind)=>{
-                    return <TableCell key={`th_${indexedDB}`} >{val}</TableCell>
+  }
+  return (
+    <div>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {
+                headers.map((val, ind) => {
+                  return <TableCell key={`th${indexedDB}`} >{val}</TableCell>
                 })
-            }
-          </TableRow>
-        <TableBody>
-          {users.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-                {
-                    columns.map((val, ind)=>{
-                        return <TableCell key={`td_${ind}`} component="th" scope="row">
-                        {row[val]}
-                      </TableCell>
-
-                    })
-                }              
-              <TableCell align="right">{row.protein}</TableCell>
+              }
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-        </div>
-    )
+          </TableHead>
+          <TableBody>
+            {users?.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                {
+                  columns?.map((val, ind) => {
+                    return <TableCell key={`td_${ind}`} component="th" scope="row">
+                      {row[val]}
+                    </TableCell>
+
+                  })
+                }
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  )
 }
 
 export default Users
