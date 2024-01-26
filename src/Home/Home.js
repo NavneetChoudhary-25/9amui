b0f340c250414f21aa4efecc8347b32f09a46874
navneet choudhary <navneetchoudhary90980@gmail.com>
2024-01-26 21:59:53 +0530

    latest

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import styles from './Home.module.css'
+import { Cookies } from '@/common/cookies';
+import { useRouter } from 'next/navigation';
 
 export const Home = () => {
+  const router = useRouter();
+  if (!Cookies.hasActiveSession()) {
+    router.push('/')
+  }
   return (
     <div>
         <h1 className='mx-5 my-5 text-center'>Welcome...</h1>  
--- src/app/users/page.js
+++ src/app/users/page.js
@@ -24,6 +24,7 @@ const Users = () => {
   }
 
   useEffect(() => {
+    console.log("users")
     fnGetUsers();
   }, [])
 
